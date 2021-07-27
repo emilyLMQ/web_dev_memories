2c19943ad921ddbd9c188d339e2b203764885378
EmilyyLu <emily.lmq960524@gmail.com>
2021-07-27 13:40:39 -0700

    console fixed

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+import { getPosts } from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -7,7 +11,11 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
-    
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch(getPosts());
+    }, [dispatch]);
     return (
         <Container maxidth = 'lg'>
             <AppBar className = {classes.appBar} position = "static" color = "inherit">
@@ -16,7 +24,7 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing = {3}>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing = {3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
