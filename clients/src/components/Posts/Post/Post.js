654e1ad2ccc70dd423ff3dacc7cfbf99f4c0bc2d
EmilyyLu <emily.lmq960524@gmail.com>
2021-07-28 17:40:24 -0700

    like function activated

--- clients/src/components/Posts/Post/Post.js
+++ clients/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
@@ -28,11 +28,12 @@ const Post = ({ post, setCurrentId }) => {
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag}`)}</Typography>
             </div>
+            <Typography className={classes.title}  gutterBottom variant="h5" component="h2">{post.title}</Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="h7" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}><ThumbUpAltIcon fontSize="small"/> Like {post.likeCount} </Button> 
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small"/> Like {post.likeCount} </Button> 
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small"/> Delete</Button>
             </CardActions>
         </Card>
